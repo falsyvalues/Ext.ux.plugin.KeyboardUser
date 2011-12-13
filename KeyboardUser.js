e16655b310ab62d1afb80276901355948d045291
Michał Lipiński <mylith@gmail.com>
2011-12-13 16:33:13 +0100

    initial commit

---
+++ KeyboardUser.js
@@ -0,0 +1,110 @@
+Ext.define('Ext.ux.plugin.field.date.KeyboardUser', {
+    extend: 'Ext.AbstractPlugin',
+    alias: 'plugin.fielddatetypinghelper',
+    requires: ['Ext.Date', 'Ext.EventObject'],
+    /**
+     * @cfg {String} token
+     * Token that represents "now"
+     */
+    token: "t",
+    /**
+     * Key number see Ext.EventObject
+     * @cfg {Number} activationKey
+     */
+    activationKey: Ext.EventObject.TAB,
+    /**
+     * Date interval constant
+     * @cfg {String} interval
+     */
+    interval: Ext.Date.DAY,
+    /**
+     * Plugin constructor
+     * @param {Object} config
+     */
+    constructor: function(config) {
+        var me = this;
+        Ext.apply(me, config);
+        if (!me.token) {
+            me.token = "t";
+        }
+        me.splitRe = new RegExp('(' + me.token + ')(\\+|\\-)?([0-9]+)?');
+    },
+    /**
+     * @private
+     * Plugin initializer
+     * @param {Ext.form.field.Date} field
+     */
+    init: function(field) {
+        var me = this;
+        Ext.apply(field, {
+            enableKeyEvents: true
+        });
+        field.addListener( 'keypress', me.help, me );
+        me.field = field;
+    },
+    /**
+     * @private
+     * Test value for pattern matching
+     * @param {Ext.form.field.Date} field
+     * @returns {Boolean} Value is ok
+     */
+    testValue: function(field) {
+        var me = this,
+            value = field.getValue() || "";
+
+        if ( !me.splitRe.test(value) ) {
+            return false;
+        }
+        return true;
+    },
+    /**
+     * @private
+     * Returns value base on pattern
+     *
+     * @param {String} token
+     * @param {String} symbol
+     * @param {String} number
+     * @returns {Date} Date object value
+     */
+    getValue: function(token, symbol, number) {
+        var me = this,
+            value = new Date(),
+            symbol = symbol ? (symbol === "-" ? -1 : 1) : null,
+            number = parseInt(number, 10) || 0;
+
+        value = new Date();
+        
+        if ( !symbol || !number && number !== 0 ) {
+            return value;
+        }
+
+        return Ext.Date.add(value, me.interval, symbol * number);
+    },
+    /**
+     * @private
+     * Filling up field under a certain condition
+     * 
+     * @param {Ext.form.field.Date} field
+     * @param {Ext.EventObject} event
+     * @param {Object} data
+     * @returns {Boolean} Value was saved
+     */
+    help: function(field, event, data) {
+        var me = this;
+
+        if (event.getKey() === me.activationKey && me.testValue(field)) {
+            field.setValue( me.getValue.apply( me, me.splitRe.exec( field.getValue().toString() ).slice(1) ) );
+        }
+
+        return true;
+    },
+    /**
+     * @method
+     * AbstractComponent calls destroy on all its plugins at destroy time.
+     */
+    destroy: function() {
+        var me = this;
+        me.field.removeListener( 'keypress', me.help, me );
+        me.callParent(arguments);
+    }
+});
---
+++ README.md
@@ -0,0 +1,38 @@
+Description
+===========
+Simple Extjs 4 datefield plugin that helps people who use the keyboard to work.
+
+In basics, type t+1 press tab and you will get date value (today + 1) in Your datefield.
+
+Config & example
+-------------
+t+1 = today + 1 day
+t-1 = today - 1 day
+
+*   token is "t"
+*   activationKey "tab" see Ext.EventObject
+*   interval DAY see Ext.Date
+
+License
+===========
+All of this is licensed under the MIT license shown below.
+
+Copyright (c) 2011 Michał Lipiński <mylith@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
